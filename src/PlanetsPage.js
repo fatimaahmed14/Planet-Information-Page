2e5b6d121f56fda0c6457bad5f6fcf99113792cd
Fatima <fxtimaahmed14@gmail.com>
2022-12-08 15:37:54 +0000

    preliminary planets created in css

--- src/PlanetsPage.js
+++ src/PlanetsPage.js
@@ -1,8 +1,15 @@
 function PlanetsPage() {
   return (
-    <>
-      <p>HI!</p>
-    </>
+    <div className="planets">
+      <div className="mercury">mercury</div>
+      <div className="venus">venus</div>
+      <div className="earth">earth</div>
+      <div className="mars">mars</div>
+      <div className="jupiter">jupiter</div>
+      <div className="saturn">saturn</div>
+      <div className="uranus">uranus</div>
+      <div className="neptune">neptune</div>
+    </div>
   );
 }
 
