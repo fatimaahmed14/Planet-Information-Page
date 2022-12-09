d5df8082e0229a0d7dde5e02d26d8ffe47edac60
Fatima <fxtimaahmed14@gmail.com>
2022-12-09 12:27:28 +0000

    data fetched with useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import StartPage from "./StartPage";
 import PlanetsPage from "./PlanetsPage";
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/planets")
+      .then((res) => res.json())
+      .then((planetData) => setPlanets(planetData));
+  }, []);
   return (
     <>
       <Routes>
         <Route path="/" element={<StartPage />} />
-        <Route path="/planets" element={<PlanetsPage />} />
+        <Route path="/planets" element={<PlanetsPage planets={planets} />} />
       </Routes>
     </>
   );
