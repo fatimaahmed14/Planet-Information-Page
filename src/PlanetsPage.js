cf429e244c7efa622d363a4439040834ae6f9a40
Fatima <fxtimaahmed14@gmail.com>
2022-12-10 16:13:50 +0000

    some styling changes

--- src/PlanetsPage.js
+++ src/PlanetsPage.js
@@ -9,20 +9,26 @@ function PlanetsPage({ planets }) {
       <Link to={`/planets/${planets.name}`}>
         <div className="venus">Venus</div>
       </Link>
-      <div className="earth">Earth</div>
-      <div className="mars">Mars</div>
-      <div className="jupiter">Jupiter</div>
-      <div className="saturn">Saturn</div>
-      <div className="uranus">Uranus</div>
-      <div className="neptune">Neptune</div>
+      <Link to={`/planets/${planets.name}`}>
+        <div className="earth">Earth</div>{" "}
+      </Link>
+      <Link to={`/planets/${planets.name}`}>
+        <div className="mars">Mars</div>
+      </Link>
+      <Link to={`/planets/${planets.name}`}>
+        <div className="jupiter">Jupiter</div>
+      </Link>
+      <Link to={`/planets/${planets.name}`}>
+        <div className="saturn">Saturn</div>
+      </Link>
+      <Link to={`/planets/${planets.name}`}>
+        <div className="uranus">Uranus</div>
+      </Link>
+      <Link to={`/planets/${planets.name}`}>
+        <div className="neptune">Neptune</div>
+      </Link>
     </div>
   );
 }
 
 export default PlanetsPage;
-
-// place links on each planet individually
-// Do I want to hard code each page ?
-// I will be using names instead of ids
-// need to re-cap how to link using ids
-// I think they require location and state but cant be too sure
