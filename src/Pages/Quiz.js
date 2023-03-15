04ff2e0db522a89f0811c6589a36340b659890a0
fatima <fxtimaahmed14@gmail.com>
2023-03-15 14:28:27 +0000

    quiz question addded

--- src/Pages/Quiz.js
+++ src/Pages/Quiz.js
@@ -3,91 +3,94 @@ import { Link } from "react-router-dom";
 
 function Quiz() {
   return (
-    <div className="quiz-container">
-      <form>
-        <div>
-          <h2>Question 1</h2>
-          <label>
-            <input type="radio" name="question1" value="option1" />
-            Option 1
-          </label>
-          <label>
-            <input type="radio" name="question1" value="option2" />
-            Option 2
-          </label>
-          <label>
-            <input type="radio" name="question1" value="option3" />
-            Option 3
-          </label>
-        </div>
+    <div className="quiz-grid">
+      <div className="quiz-title">Planet Quiz</div>
+      <div className="quiz-container">
+        <form>
+          <div>
+            <h2>Which planet is closest to the sun ?</h2>
+            <label>
+              <input type="radio" name="question1" value="option1" />
+              Earth
+            </label>
+            <label>
+              <input type="radio" name="question1" value="option2" />
+              Mars
+            </label>
+            <label>
+              <input type="radio" name="question1" value="option3" />
+              Mercury
+            </label>
+          </div>
 
-        <div>
-          <h2>Question 2</h2>
-          <label>
-            <input type="radio" name="question2" value="option1" />
-            Option 1
-          </label>
-          <label>
-            <input type="radio" name="question2" value="option2" />
-            Option 2
-          </label>
-          <label>
-            <input type="radio" name="question2" value="option3" />
-            Option 3
-          </label>
-        </div>
-        <div>
-          <h2>Question 3</h2>
-          <label>
-            <input type="radio" name="question3" value="option1" />
-            Option 1
-          </label>
-          <label>
-            <input type="radio" name="question3" value="option2" />
-            Option 2
-          </label>
-          <label>
-            <input type="radio" name="question3" value="option3" />
-            Option 3
-          </label>
-        </div>
-        <div>
-          <h2>Question 4</h2>
-          <label>
-            <input type="radio" name="question4" value="option1" />
-            Option 1
-          </label>
-          <label>
-            <input type="radio" name="question4" value="option2" />
-            Option 2
-          </label>
-          <label>
-            <input type="radio" name="question4" value="option3" />
-            Option 3
-          </label>
-        </div>
-        <div>
-          <h2>Question 5</h2>
-          <label>
-            <input type="radio" name="question5" value="option1" />
-            Option 1
-          </label>
-          <label>
-            <input type="radio" name="question5" value="option2" />
-            Option 2
-          </label>
-          <label>
-            <input type="radio" name="question5" value="option3" />
-            Option 3
-          </label>
-        </div>
+          <div>
+            <h2>which planet has the most moons ?</h2>
+            <label>
+              <input type="radio" name="question2" value="option1" />
+              Venus
+            </label>
+            <label>
+              <input type="radio" name="question2" value="option2" />
+              Uranus
+            </label>
+            <label>
+              <input type="radio" name="question2" value="option3" />
+              Saturn
+            </label>
+          </div>
+          <div>
+            <h2>Which planet is the coldest ?</h2>
+            <label>
+              <input type="radio" name="question3" value="option1" />
+              Saturn
+            </label>
+            <label>
+              <input type="radio" name="question3" value="option2" />
+              Neptune
+            </label>
+            <label>
+              <input type="radio" name="question3" value="option3" />
+              Mars
+            </label>
+          </div>
+          <div>
+            <h2>Which planet is red ?</h2>
+            <label>
+              <input type="radio" name="question4" value="option1" />
+              Mars
+            </label>
+            <label>
+              <input type="radio" name="question4" value="option2" />
+              Mercury
+            </label>
+            <label>
+              <input type="radio" name="question4" value="option3" />
+              Jupiter
+            </label>
+          </div>
+          <div>
+            <h2>Which planet do we live on ?</h2>
+            <label>
+              <input type="radio" name="question5" value="option1" />
+              Neptune
+            </label>
+            <label>
+              <input type="radio" name="question5" value="option2" />
+              Earth
+            </label>
+            <label>
+              <input type="radio" name="question5" value="option3" />
+              Venus
+            </label>
+          </div>
 
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit">Submit</button>
+        </form>
 
-      <button className="go-back-button">
-        <Link to="/planets">GO BACK</Link>
-      </button>
+        <button className="go-back-button">
+          <Link to="/planets">GO BACK</Link>
+        </button>
+      </div>
     </div>
   );
 }
